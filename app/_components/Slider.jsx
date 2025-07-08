b74ff60d65b73663ab0478ca4d33d680bd9240fa
Souravdutta09 <sourav.academics09@gmail.com>
2025-07-09 01:29:58 +0530

    slider updates

--- app/_components/Slider.jsx
+++ app/_components/Slider.jsx
@@ -14,25 +14,19 @@ function Slider({ sliderList }) {
       <Carousel>
         <CarouselContent>
           {sliderList.map((item, index) => {
-            const imageUrl =
-             
-              (
-                item.image?.formats?.small?.url ||  // Use "small" version if available
-                item.image?.url ||                  // Fallback to original image
-                ''
-              );
-
             return (
               <CarouselItem key={item.id || index}>
-                {imageUrl && (
-                  <Image
-                    src={imageUrl}
-                    alt={item.name}
-                    width={1000}
-                    height={300}
-                    className="object-cover rounded-2xl w-full h-[200px] md:h-[450px] "
-                  />
-                )}
+
+                <Image
+                  src={item.image?.formats?.small?.url ||  // Use "small" version if available
+                    item.image?.url ||                  // Fallback to original image
+                    ''}
+                  width={1000}
+                  height={300}
+                  alt="imageName"
+                  className="object-cover rounded-2xl w-full h-[200px] md:h-[400px] "
+                />
+
               </CarouselItem>
             );
           })}
